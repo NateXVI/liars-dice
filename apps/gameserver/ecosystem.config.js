40c2475e329bc051d11d382f81ddc5b1a49e85f1
Nate Diven <natediven1@gmail.com>
2023-10-29 20:33:01 -0600

    limit to one instance

--- apps/gameserver/ecosystem.config.js
+++ apps/gameserver/ecosystem.config.js
@@ -6,7 +6,8 @@ module.exports = {
         script: "dist/index.js",
         time: true,
         watch: false,
-        instances: os.cpus().length,
+        // instances: os.cpus().length,
+        instances: 1,
         exec_mode: 'fork',
         wait_ready: true,
         env_production: {
