ac1ec907977d7165842d4a998c8ed6b7a4743e96
Nate Diven <natediven1@gmail.com>
2023-10-31 15:58:45 -0600

    updated scene transitions

--- apps/frontend/src/lib/components/scenes/SceneContainer.svelte
+++ apps/frontend/src/lib/components/scenes/SceneContainer.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-	import { elasticOut, quadIn } from 'svelte/easing';
+	import { fly, scale } from 'svelte/transition';
+	import { quadIn, quadOut } from 'svelte/easing';
 	import classNames from '$lib/utils/classNames';
 
 	let clazz: string = '';
 	export { clazz as class };
 	let height = 100;
-	const inDuration = 1200;
+	const inDuration = 400;
 	const outDuration = 300;
 </script>
 
 <div
-	class="{classNames('fixed top-0 left-0 right-0 bottom-0 h-screen w-screen overflow-auto', clazz)}"
-	in:fly="{{ y: height, duration: inDuration, delay: outDuration, easing: elasticOut }}"
+	class="{classNames('fixed bottom-0 left-0 right-0 top-0 h-screen w-screen overflow-auto', clazz)}"
+	in:scale="{{ start: 0.5, duration: inDuration, delay: 150, easing: quadOut }}"
 	out:fly="{{ y: height, duration: outDuration, easing: quadIn }}"
 >
 	<slot />
