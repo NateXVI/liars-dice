696a7e8ceb14cd920213992ffde2c1e0ac614866
Nate Diven <natediven1@gmail.com>
2023-10-31 13:33:42 -0600

    removed unused rolling dice message

--- apps/frontend/src/lib/components/scenes/Table/TableStates.svelte
+++ apps/frontend/src/lib/components/scenes/Table/TableStates.svelte
@@ -8,11 +8,7 @@
 </script>
 
 <div class="w-full flex-1">
-	{#if $state.tableState === 'rolling-dice'}
-		<div class="flex h-full items-center justify-center font-bold text-white">
-			<p class="text-4xl">Rolling Dice</p>
-		</div>
-	{:else if $state.tableState === 'playing'}
+	{#if $state.tableState === 'playing'}
 		{#if $state.currentTurn === $room.sessionId}
 			<Controls />
 		{:else}
