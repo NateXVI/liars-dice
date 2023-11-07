1e0ef8176ecf431ae0ee64210fa4ca0495a6b4a3
Nate Diven <natediven1@gmail.com>
2023-11-06 20:38:07 -0700

    updated favicon and added title and description

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -2,4 +2,12 @@
 	import Game from '$lib/components/Game.svelte';
 </script>
 
+<svelte:head>
+	<title>Liar's Dice</title>
+	<meta
+		name="description"
+		content="Master the art of deception in our online Liars Dice game! Team up with friends for a captivating multiplayer experience and outsmart your opponents in this classic game of bluff, directly in your browser."
+	/>
+</svelte:head>
+
 <Game />
